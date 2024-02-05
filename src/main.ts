1831f6a0d4a21b82f7a16db53be744dd2e60766d
Ryan Parsons <ryanp6441@gmail.com>
2024-02-04 21:14:10 -0800

    adds necessary packages

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueApexCharts from 'vue3-apexcharts'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+const app = createApp(App)
+
+app.use(VueApexCharts)
+
+app.use(vuetify)
+
+app.mount('#app')
